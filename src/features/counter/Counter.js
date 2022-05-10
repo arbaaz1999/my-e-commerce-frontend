1c6ab76269b5c2986c48408b506f874421d01cd6
yahiyamansuri14 <yahiyamansuri14@gmail.com>
2022-05-11 00:07:29 +0530

    changes made in reducer

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -5,13 +5,12 @@ import {
   increment,
   incrementByAmount,
   incrementAsync,
-  incrementIfOdd,
-  selectCount,
+  incrementIfOdd
 } from './counterSlice';
 import styles from './Counter.module.css';
 
 export function Counter() {
-  const count = useSelector(selectCount);
+  const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
   const [incrementAmount, setIncrementAmount] = useState('2');
 
